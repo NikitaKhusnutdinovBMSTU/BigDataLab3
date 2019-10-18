c88ca1de135f3639f3b388f52ce7a4580fb86cb1
Nikita Khusnutdinov <glaider102@mail.ru>
2019-10-18 21:10:19 +0300

    Scripted auto-commit on change (2019-10-18 21:10:19) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab3/AirportPair.java
+++ src/main/java/khusnutdinov/bmstu/lab3/AirportPair.java
@@ -1,4 +1,37 @@
 package khusnutdinov.bmstu.lab3;
 
-public class AirportPair {
+import java.io.Serializable;
+
+public class AirportPair implements Serializable {
+    private int totalFlights;
+    private int cancelledFlights;
+    private int delayedFlights;
+    private double maxDelay;
+
+    public AirportPair(int totalFlights, int cancelledFlights, int delayedFlights, double maxDelay){
+        this.totalFlights = totalFlights;
+        this.cancelledFlights = cancelledFlights;
+        this.delayedFlights = delayedFlights;
+        this.maxDelay = maxDelay;
+    }
+
+    public static AirportPair addData(AirportPair airportPair1, AirportPair airportPair2){
+        return new AirportPair(
+                airportPair1.totalFlights + airportPair2.totalFlights,
+                airportPair1.cancelledFlights + airportPair2.cancelledFlights,
+                airportPair1.delayedFlights + airportPair2.delayedFlights,
+                Math.max(airportPair1.maxDelay, airportPair2.maxDelay)
+        );
+    }
+
+    public  AirportPair addFlight(int cancelledFlight, int delayedFlight, double maxDelay){
+        return new AirportPair(
+                this.totalFlights + 1,
+                this.cancelledFlights + cancelledFlight,
+                this.delayedFlights + delayedFlight,
+                Math.max(this.maxDelay, maxDelay)
+        );
+    }
+
+    
 }

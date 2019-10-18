5c5710cd315414655c0048d016851a7c2d97d1e9
Nikita Khusnutdinov <glaider102@mail.ru>
2019-10-18 20:25:24 +0300

    Scripted auto-commit on change (2019-10-18 20:25:24) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab3/FlightsPercentCanceled.java
+++ src/main/java/khusnutdinov/bmstu/lab3/FlightsPercentCanceled.java
@@ -1,9 +1,20 @@
 package khusnutdinov.bmstu.lab3;
 
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class FlightsPercentCanceled {
     SparkConf sparkConf = new SparkConf();
-    ff3fgs
+    JavaSparkContext sc = new JavaSparkContext(sparkConf);
+
+    JavaRDD<String> flightsData = sc.textFile("Flights");
+    JavaRDD<String> airportsData = sc.textFile("Airports");
+
+
 }

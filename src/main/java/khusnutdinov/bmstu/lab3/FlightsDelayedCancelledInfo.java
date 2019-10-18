f03a2b6f3d7652628ff2502b94816ea4620e0c46
Nikita Khusnutdinov <glaider102@mail.ru>
2019-10-18 21:27:12 +0300

    Scripted auto-commit on change (2019-10-18 21:27:12) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab3/FlightsDelayedCancelledInfo.java
+++ src/main/java/khusnutdinov/bmstu/lab3/FlightsDelayedCancelledInfo.java
@@ -8,33 +8,50 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 
+import java.util.Map;
+
 public class FlightsDelayedCancelledInfo {
-    SparkConf sparkConf = new SparkConf();
-    JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
-    JavaRDD<String> flightsCSV = sc.textFile("~/Desktop/hadoop/labs/2/664600583_T_ONTIME_sample.csv");
-    JavaRDD<String> airportsCSV = sc.textFile("~/Desktop/hadoop/labs/2/L_AIRPORT_ID.csv");
+    public static void main(String[] args) {
+        SparkConf sparkConf = new SparkConf();
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
-    JavaPairRDD<Tuple2, FlightsData> flightsData = flightsCSV.mapToPair(in -> {
-        FlightsData flData = new FlightsData(in);
-        return new Tuple2<>(new Tuple2<>(flData.getOriginAirportID(), flData.getDestAirportID()), flData);
-    });
+        JavaRDD<String> flightsCSV = sc.textFile("~/Desktop/hadoop/labs/2/664600583_T_ONTIME_sample.csv");
+        JavaRDD<String> airportsCSV = sc.textFile("~/Desktop/hadoop/labs/2/L_AIRPORT_ID.csv");
 
-    JavaPairRDD<Tuple2, AirportPair> combineFlData = flightsData.combineByKey(
-            airport -> new AirportPair(
-                    1,
-                    airport.getCancelled(),
-                    airport.getDelayed(),
-                    airport.getDelay()
-            ),
-            (pair, airport) -> pair.addFlight(
-                    airport.getCancelled(),
-                    airport.getDelayed(),
-                    airport.getDelay()
-            ),
-            AirportPair::addData
-    );
+        JavaPairRDD<Tuple2, FlightsData> flightsData = flightsCSV.mapToPair(in -> {
+            FlightsData flData = new FlightsData(in);
+            return new Tuple2<>(new Tuple2<>(flData.getOriginAirportID(), flData.getDestAirportID()), flData);
+        });
+
+        JavaPairRDD<Tuple2, AirportPair> combineFlData = flightsData.combineByKey(
+                airport -> new AirportPair(
+                        1,
+                        airport.getCancelled(),
+                        airport.getDelayed(),
+                        airport.getDelay()
+                ),
+                (pair, airport) -> pair.addFlight(
+                        airport.getCancelled(),
+                        airport.getDelayed(),
+                        airport.getDelay()
+                ),
+                AirportPair::addData
+        );
+
+        Map<Integer, String> pair = airportsCSV.mapToPair(
+                row -> {
+                    String[] table = row.split(",");
+                    return new Tuple2<>(Integer.parseInt(table[0]), table[1]);
+                }
+        ).collectAsMap();
+
+        final Broadcast<Map<Integer, String>> broadcast = sc.broadcast(pair);
+
+        System.out.println(combineFlData);
+    }
 
 }
